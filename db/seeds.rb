1b05cb5cb2d0cef14688ab0023ab175f03060350
Martin d'Allens <martin.dallens@gmail.com>
2020-01-19 18:48:28 +0100

    Add development instructions, add room seeds, simplify colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+buildings = Building.create([{ name: 'Building1', city: 'City1' }])
+Room.create([{ name: 'Room1', building_id: buildings.first.id, hid: 'fake_hid_1', color: '#467fcf' }])
+Room.create([{ name: 'Room2', building_id: buildings.first.id, hid: 'fake_hid_2', color: '#5eba00' }])
+Room.create([{ name: 'Room3', building_id: buildings.first.id, hid: 'fake_hid_3', color: '#cd201f' }])
+Room.create([{ name: 'Room4', building_id: buildings.first.id, hid: 'fake_hid_4', color: '#343a40' }])
+Room.create([{ name: 'Room5', building_id: buildings.first.id, hid: 'fake_hid_5', color: '#f1c40f' }])
