7a7b34479d27790404276b9819322ffeca4a8629
Julien Chardon <julien@ilek.fr>
2020-02-18 21:07:44 +0100

    Change slack Oauth scope to fix user creation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :slack, ENV['SLACK_APP_ID'], ENV['SLACK_APP_SECRET'], scope: 'im:write', user_scope: 'identity.basic,identity.email,identity.avatar,identity.team'
+  config.omniauth :slack, ENV['SLACK_APP_ID'], ENV['SLACK_APP_SECRET'], scope: 'identity.basic,identity.email,identity.avatar,identity.team'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
